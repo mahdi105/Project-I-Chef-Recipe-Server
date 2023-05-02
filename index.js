0bd6ae0d13b249a953b95e9cd1ce2c4c474c31f0
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-02 20:43:11 +0600

    Created and setup  express.js server

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 app.use(cors());
+// home 
+app.get('/',(req,res) => {
+    res.send('Recipes Server is running');
+})
+
+
+app.listen(port, () =>{
+    console.log("The recipes server is running in the port:", port);
+})
