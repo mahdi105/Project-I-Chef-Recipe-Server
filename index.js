c9370a75b32a01b092cd10b55b4564dd7cadda1f
Mahdi Hasan <mahdihasan10599@gmail.com>
2023-05-02 20:57:24 +0600

    Built chef, recipes and chef recipes API

--- index.js
+++ index.js
@@ -2,10 +2,27 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
+const chefs = require('./data/chefs.json');
+const recipes = require('./data/recipes.json');
 app.use(cors());
 // home 
 app.get('/',(req,res) => {
     res.send('Recipes Server is running');
+})
+
+// Chefs API
+app.get('/chefs',(req,res)=> {
+    res.send(chefs);
+})
+// All Recipes API
+app.get('/recipes',(req,res)=>{
+    res.send(recipes);
+})
+// Certain Chef Recipes by Id API
+app.get('/chef/recipes/:id',(req,res) => {
+    const id = req.params.id;
+    const chefRecipes = recipes.filter(recipe => recipe.chef_id === id);
+    res.send(chefRecipes);
 })
 
 
